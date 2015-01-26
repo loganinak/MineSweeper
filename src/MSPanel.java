4d70fb7e8d1bd379ae7a5e2df50600303c2ff803
loganinak <wakeup2early@gmail.com>
2015-01-26 01:32:52 -0700

    Started using GitHub

---
+++ src/MSPanel.java
@@ -0,0 +1,172 @@
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+
+/**
+ * This class is made to be instanced many times to make keeping track of clicks, flags, mines, and the number of mines around it super simple. 
+ * For example instead of figuring out if a box was clicked based on the position of the mouse, each box will be a JPanel the will automatically know if it's been clicked.
+ * @author Logan
+ */
+public class MSPanel extends JPanel {
+
+    //instance variables
+    boolean clicked = false;
+    boolean mine;
+    boolean flagged = false;
+    String mineNumber;
+    int xCord;
+    int yCord;
+
+    //Constructor
+    /**
+     * This is the constructor for the MSPanel Class. 
+     * The X and Y values of its position are needed to figure out where to check for flooding.
+     * @param inX The X position of this particular panel
+     * @param inY the Y position of this particular panel
+     */
+        public MSPanel(int inX, int inY) {
+        xCord = inX;
+        yCord = inY;
+        setPreferredSize(new Dimension(40, 40));
+        addMouseListener(new MSListener());
+
+        repaint();
+    }
+
+    //The all important paint method
+    @Override
+    public void paint(Graphics g) {
+        //changes font and gets prepared for centering
+        Font f = new Font("Times", Font.PLAIN, 20);
+        g.setFont(f);
+        FontMetrics fm = g.getFontMetrics();
+        int a = fm.getAscent();
+        int h = fm.getHeight();
+        //paints an unclicked looking box if it hasn't been clicked on
+        if (clicked == false) {
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, 40, 40);
+            g.setColor(new Color(179, 173, 143));
+            g.fillRect(0, 0, 38, 38);
+            //paints a ! if it's been flagged
+            if (flagged == true) {
+                g.setColor(Color.RED);
+                int w = fm.stringWidth("!");
+                g.drawString("!", 20 - w / 2, 20 + a - h / 2);
+            }
+            //paints a red box if a mine is in that spot
+        } else if (clicked == true && mine == true) {
+            g.setColor(Color.RED);
+            g.fillRect(0, 0, 40, 40);
+            //paints a clicked looking box and the number of mines nearby
+        } else if (clicked == true) {
+            int w = fm.stringWidth(mineNumber);
+            g.setColor(new Color(186, 185, 177));
+            g.fillRect(0, 0, 40, 40);
+            g.setColor(Color.BLUE);
+            g.drawString(mineNumber, 20 - w / 2, 20 + a - h / 2);
+        }
+    }
+
+    //user input stuff
+    private class MSListener implements MouseListener {
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            //System.out.println("press");
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            //System.out.println("release");
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            //System.out.println("mouse entered");
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            //System.out.println("mouse exited");
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            //right click
+            if (SwingUtilities.isRightMouseButton(e)) {
+                System.out.println("right Click");
+                flagged = !flagged;
+                repaint();
+            }
+            //left click
+            if (SwingUtilities.isLeftMouseButton(e) && !flagged) {
+                System.out.println("left click");
+                clicked = true;
+                repaint();
+                //floods the board if there is no mines around it
+                if ("".equals(mineNumber)) {
+                    MineSweeper.flood(xCord, yCord);
+                }
+                //makes the player lose if there is a mine
+                if (mine == true) {
+                    MineSweeper.win(false);
+                }
+                //checks for a win and makes the player win if ther
+                if (MineSweeper.checkOver() == true) {
+                    MineSweeper.win(true);
+                }
+            }
+
+        }
+    }
+
+    //Method needed for counting the mines around a particular spot(returns 0 or 1 based on the boolean value of mine)
+    public int getMine() {
+        if (mine == true) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    //Method needed to check for game over
+    public boolean getClicked() {
+        return clicked;
+    }
+
+    //returns the number of mines surrounding it. Needed for the flood method
+    public int getNum() {
+        if ("".equals(mineNumber)) {
+            return 0;
+        } else {
+            return Integer.parseInt(mineNumber);
+        }
+    }
+
+    //needed to change the value of mineNumber after setting the mines
+    public void setNumber(int inNum) {
+        if (inNum == 0) {
+            mineNumber = "";
+        } else {
+            mineNumber = Integer.toString(inNum);
+        }
+    }
+
+    //Needed to change clicked when flooding when flooding
+    public void setClicked() {
+        System.out.println("virtual click");
+        clicked = true;
+        repaint();
+        //floods the area around it
+        MineSweeper.flood(xCord, yCord);
+    }
+
+    //method for randomly deciding if a mine should be placed or not
+    public void layMines() {
+        mine = (int) (Math.random() * 10) == 0;
+    }
+
+}
---
+++ src/MineSweeper.java
@@ -0,0 +1,105 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This is the Main class for my Mine Sweeper game. 
+ * This class is responsible for logic that needs information from or affects all the boxes on the mineSweeper JFrame.
+ * @author Logan
+ */
+
+public class MineSweeper extends JFrame {
+
+    static MSPanel[][] board;
+
+    /**
+     * Constructor method for the MineSweeper Class
+     * @param width width of the board
+     * @param height height of the board
+     */
+    public MineSweeper(int width, int height) {
+        super("MineSweeper");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new GridLayout(width, height, 1, 1));
+
+        //builds the board
+        board = new MSPanel[width][height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                board[x][y] = new MSPanel(x, y);
+                board[x][y].layMines();
+                add(board[x][y]);
+            }
+        }
+        
+        //sets mineNum from the MSPanel class
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int count = 0;
+                //looks and counts the mines in the surrounding areas
+                for (int up = -1; up <= 1; up++) {
+                    for (int side = -1; side <= 1; side++) {
+                        if ((x + up < 0) || (x + up > width - 1) || (y + side < 0) || (y + side > height - 1)) {
+                        } else {
+                            count += board[x + up][y + side].getMine();
+                        }
+                    }
+                }
+                board[x][y].setNumber(count);
+            }
+        }
+
+        //sets the window size, makes the window visible, and locks the window size
+        pack();
+        setVisible(true);
+        setResizable(false);
+    }
+
+    //checks if the player has won the game or not
+    public static boolean checkOver() {
+        int count = 0;
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[0].length; y++) {
+                if (board[x][y].getClicked() == true || board[x][y].getMine() == 1) {
+                    count++;
+                }
+            }
+        }
+        if (count == board.length * board[0].length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //floods the board if the player clicks on a spot with zero mines surrounding it
+    public static void flood(int x, int y) {
+        if (board[x][y].getNum() == 0) {
+            for (int up = -1; up <= 1; up++) {
+                for (int side = -1; side <= 1; side++) {
+                    if ((x + up < 0) || (x + up > board.length - 1) || (y + side < 0) || (y + side > board[0].length - 1)) {
+                    } else if (board[x + up][y + side].getClicked() == false) {
+                        board[x + up][y + side].setClicked();
+                    }
+                }
+            }
+        }
+    }
+
+    //method for stopping the game if the player won or lost
+    public static void win(boolean win) {
+        if (win == true) {
+            JOptionPane.showMessageDialog(null, "You Won!");
+            System.exit(0);
+        } else {
+            JOptionPane.showMessageDialog(null, "You Hit a Mine :(");
+            System.exit(0);
+        }
+    }
+
+    //starts the game
+    public static void main(String[] args) {
+        MineSweeper game = new MineSweeper(10, 10);
+    }
+
+}
